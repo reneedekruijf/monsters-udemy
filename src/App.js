85bf69def3d70da575d2bf1d094a96e2f75e5256
Renee de Kruijf <renee@webdesignpuntnl.com>
2020-07-05 14:35:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map((monster, key) => <h1 key={key}>{monster.name}</h1>)
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
